08404478839875f3f63bf74ff86b3fcc1f833e21
Tenpi <pie4chan@gmail.com>
2022-05-06 07:29:27 -0400

    fixes

--- structures/Functions.ts
+++ structures/Functions.ts
@@ -854,6 +854,7 @@ export default class Functions {
     }
 
     public static formatDate(date: Date) {
+        if (!date) return ""
         let year = date.getFullYear()
         let month = (1 + date.getMonth()).toString().padStart(2, "0")
         let day = date.getDate().toString().padStart(2, "0")
